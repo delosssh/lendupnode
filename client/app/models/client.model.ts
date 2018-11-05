ac2a02fb8e1f3274f898c7890c8756f032214ac6
delosssh <delosssh@gmail.com>
2018-11-05 08:12:34 +0300

    updated

--- client/app/models/client.model.ts
+++ client/app/models/client.model.ts
@@ -6,6 +6,9 @@ export class ClientModel extends BaseModel {
   middleName: String = '';
   lastName: String = '';
   surName: String = '';
+  gender: String = '';
+  mobilePhone: String = '';
+  emailAddress: String = '';
   birthdate: Date = new Date();
   address1: String = '';
   address2: String = '';
@@ -13,6 +16,16 @@ export class ClientModel extends BaseModel {
   barangayName: String = '';
   cityName: String = '';
   provinceName: String = '';
+  locationMap: String = '';
+  employment: String = '';
+  businessName: String = '';
+  businessAddress: String = '';
+  businessPhone: String = '';
+  businessJobTitle: String = '';
+  grossMonthlyIncome: Number = 0;
+  businessJoiningDate: Date = new Date();
+  businessPayDate1: Date = new Date();
+  businessPayDate2: Date = new Date();
   lastUpdate: Date = new Date();
   creationDate: Date = new Date();
   loans: Number = 0;

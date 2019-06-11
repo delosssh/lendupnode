3214254b6a25210d829e09f6a87ba1d0ba76a18c
delosssh <delosssh@gmail.com>
2019-06-11 19:49:29 +0300

    updated

--- dist/server/models/dailyPaymentSchedule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var DailyPaymentSchedule = (function () {
-    function DailyPaymentSchedule() {
-    }
-    return DailyPaymentSchedule;
-}());
-exports.DailyPaymentSchedule = DailyPaymentSchedule;
-//# sourceMappingURL=dailyPaymentSchedule.js.map
